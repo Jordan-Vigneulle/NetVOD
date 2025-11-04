8f8c3802fa5f76eb9a7e8271b39c076da50043ed
Jordan Vigneulle <e76320u@etu.univ-lorraine.fr>
2025-11-04 11:00:12 +0100

    Ajout d'un bouton mettre en favori, pour les utilisateur connecté

--- src/classes/action/DisplayCatalogue.php
+++ src/classes/action/DisplayCatalogue.php
@@ -22,6 +22,10 @@ class DisplayCatalogue extends Action{
                     $html .= "<h3>{$cat['titre']}</h3>";
                     $html .= "<div class='card-actions'>";
                     $html .= "<a href='?action=display-action&playlist_id={$id}' class='btn-view-playlist'>Lecture</a>";
+                    if($_SESSION['user']){
+                        $html .= "<br><br>";
+                        $html .= "<a href='' class='btn-fav'>Mettre en favori</a>";
+                    }
                     $html .= "</div>";
                     $html .= "</div>";
                 }
